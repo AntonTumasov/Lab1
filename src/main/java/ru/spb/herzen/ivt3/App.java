d7a4fec03c69da51207def0c7af87b6faf9035f5
AntonTumasov <multibutterbread@gmail.com>
2015-10-02 12:24:17 +0300

    Update App.java
    Added new output information

--- src/main/java/ru/spb/herzen/ivt3/App.java
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -6,8 +6,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        RandomNameGenerator rnd = new RandomNameGenerator(2);
         System.out.printLn("It's me, Anton!")
+        RandomNameGenerator rnd = new RandomNameGenerator(2);
         for (int i=0;i<15;i=i+1) {
             String name = rnd.next();
             System.out.println("Hello," + name);
