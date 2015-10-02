afc3af5e231960aadac5e7f8af20ec01a7460588
AntonTumasov <multibutterbread@gmail.com>
2015-10-02 12:23:41 +0300

    Update App.java

--- src/main/java/ru/spb/herzen/ivt3/App.java
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -2,16 +2,12 @@ package ru.spb.herzen.ivt3;
 
 import org.kohsuke.randname.RandomNameGenerator;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         RandomNameGenerator rnd = new RandomNameGenerator(2);
-
+        System.out.printLn("It's me, Anton!")
         for (int i=0;i<15;i=i+1) {
             String name = rnd.next();
             System.out.println("Hello," + name);
